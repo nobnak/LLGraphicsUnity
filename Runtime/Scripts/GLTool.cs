1de3060f44babec79a5ddab043ba6e15e0566851
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-06-08 20:22:34 +0900

    Unity Math データ対応

--- Runtime/Scripts/GLTool.cs
+++ Runtime/Scripts/GLTool.cs
@@ -2,6 +2,7 @@ using LLGraphicsUnity.Shapes;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace LLGraphicsUnity {
@@ -9,14 +10,18 @@ namespace LLGraphicsUnity {
 	public static class GLTool {
 
 
-		public static void DrawLineStrip(this IEnumerable<Vector3> vertices) {
+		public static void LineStrip(this IEnumerable<float3> vertices) {
 			GL.Begin(GL.LINE_STRIP);
 			foreach (var v in vertices)
 				GL.Vertex(v);
 			GL.End();
 		}
-		public static void DrawLineStrip(this IEnumerable<Vector2> vertices, float z = 0f)
-			=> vertices.Select(v => new Vector3(v.x, v.y, z)).DrawLineStrip();
+		public static void LineStrip(this IEnumerable<float2> vertices, float z = 0f)
+			=> vertices.Select(v => new float3(v, z)).LineStrip();
+		public static void LineStrip(this IEnumerable<Vector3> vertices)
+			=> vertices.Cast<float3>().LineStrip();
+		public static void LineStrip(this IEnumerable<Vector2> vertices, float z = 0f)
+			=> vertices.Select(v => new float3(v, z)).LineStrip();
 
 		public static Matrix4x4 SquareOrtho(float aspect, float height = 1f)
 			=> Matrix4x4.Ortho(0f, aspect * height, 0f, height, 1f, -100f);
